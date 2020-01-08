3fd50ee17debc99692d1337681ce6ac461b6dc6c
margaiwangara <margaiwangara@gmail.com>
2020-01-08 19:23:53 +0300

    added back button for details screen to go back to previous screen, added styling for bottom part

--- lib/ui/details.dart
+++ lib/ui/details.dart
@@ -32,7 +32,7 @@ class _ArticleDetailsState extends State<ArticleDetails> {
                   bottomRight: Radius.circular(25.0)),
               boxShadow: [
                 BoxShadow(
-                    color: Theme.of(context).accentColor.withOpacity(0.6),
+                    color: Colors.black.withOpacity(0.6),
                     offset: new Offset(-0.75, 5.0),
                     blurRadius: 5.5)
               ],
@@ -45,24 +45,116 @@ class _ArticleDetailsState extends State<ArticleDetails> {
                       Colors.black.withOpacity(0.6), BlendMode.darken))),
         ),
         Positioned.fill(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[_topSectionHeader(), _topSectionFooter()],
-        ))
+            child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[_topSectionHeader(), _topSectionFooter()],
+                )))
       ],
     );
   }
 
   _topSectionHeader() {
-    return Container(
-      width: 60.0,
-      height: 60.0,
-      color: Colors.green,
+    return Row(
+      children: <Widget>[
+        InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Icon(
+                  Icons.chevron_left,
+                  size: 25.0,
+                  color: Colors.grey[400],
+                ),
+                Text(
+                  'Back',
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.grey[400],
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 0.9),
+                )
+              ],
+            ))
+      ],
     );
   }
 
   _topSectionFooter() {
-    return Container(width: 60.0, height: 60.0, color: Colors.pink);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(5.0),
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+          ),
+          child: Text(
+            "php > laravel".toUpperCase(),
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).accentColor,
+              letterSpacing: 0.9,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 5.0,
+        ),
+        Text(
+          'Spread Operator for Arrays Coming to PHP 7.4',
+          style: TextStyle(
+              fontSize: 20.0,
+              letterSpacing: 0.9,
+              color: Colors.white.withOpacity(0.75),
+              fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          height: 5.0,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+                height: 30.0,
+                child: Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                  Container(
+                    width: 30.0,
+                    decoration: BoxDecoration(
+                        color: Colors.teal,
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.grey[400], width: 1.5),
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://source.unsplash.com/300x300/?people"),
+                            fit: BoxFit.cover,
+                            alignment: Alignment.center)),
+                  ),
+                  SizedBox(
+                    width: 5.0,
+                  ),
+                  Text('Jane Doe',
+                      style: TextStyle(
+                          color: Colors.grey[400],
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 0.9))
+                ])),
+            Text(
+              "2 Days Ago",
+              style: TextStyle(
+                  color: Colors.grey[400],
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.9),
+            )
+          ],
+        )
+      ],
+    );
   }
 }
