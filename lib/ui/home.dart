5923a5d3945d20d5efc73edaf0eab4befe8c93db
margaiwangara <margaiwangara@gmail.com>
2020-01-23 16:00:22 +0300

    modifications to home

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -14,7 +14,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    article = fetchArticle();
+    article = fetchArticles("sort=-datePublished");
   }
 
   @override
