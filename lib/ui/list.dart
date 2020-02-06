3289dfc8b9d14d14fa76cc7f60b1d7cf1dc886b9
margaiwangara <margaiwangara@gmail.com>
2020-02-06 20:57:29 +0300

    added back inkwell to navigate back to previous page

--- lib/ui/list.dart
+++ lib/ui/list.dart
@@ -24,12 +24,40 @@ class _ArticleListState extends State<ArticleList> {
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisSize: MainAxisSize.max,
-                children: <Widget>[Text('Header Text'), Text('Body Text')],
+                children: <Widget>[_pageHeader(), Text('Body Text')],
               ),
             )
           ],
         ),
       );
     }));
+  }
+
+  _pageHeader() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        InkWell(
+            onTap: () {},
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Icon(
+                  Icons.chevron_left,
+                  size: 25.0,
+                  color: Colors.grey[400],
+                ),
+                Text(
+                  'Back',
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.grey[400],
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 0.9),
+                )
+              ],
+            ))
+      ],
+    );
   }
 }
